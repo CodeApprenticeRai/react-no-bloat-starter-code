72203620d0de5ea9c29590a82a8921ad6f8f4adb
Tare <tare.dgg@gmail.com>
2018-12-09 19:08:46 -0500

    removed / added things to make useability more general.

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-// const express = require('express');
-// const path = require('path');
-// var ipsum = require('lorem-ipsum');
-// const app = express();
-//
-// app.engine('html', require('ejs').renderFile);
-// // Cannot serve HTML with express.static()
-// //app.use( express.static( path.join(__dirname, '/views') ) );
-// app.set('view engine', 'html');
-// //app.use( express.static('public') );
-//
-//
-// app.use('/static', express.static( path.join(__dirname, '/public') ) );
-//
-// app.get('/', (req, res) => res.render('layout', { ipsum: ipsum({ count: 10, units: 'sentences'}) } ) );//, { root: __dirname + "/views", } )  );
-// app.get('/hello_ejs', (req, res) => res.render('hello_ejs', {name : Math.floor( Math.random() * 100 ) }));
-// app.get('/hello', (req, res) => res.render('example_index') );
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
-//
-//
-//
-// module.exports = app;
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.set('view engine', 'html');
+//app.use( express.static('public') );
+
+app.use('/static', express.static( path.join(__dirname, '/public') ) );
+
+app.get('/', res.send('<p>Hello : )</p>') )
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
+
+
+
+module.exports = app;
