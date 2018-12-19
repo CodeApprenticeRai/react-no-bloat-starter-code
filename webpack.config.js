81e20deb612edad56ac5b0403a077e171be899c5
Tare <tare.dgg@gmail.com>
2018-12-19 09:19:39 -0500

    more generalities

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,17 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test:/\.css$/,
+          use:[ 'style-loader', 'css-loader']
         }
-      }
     ]
   },
   plugins: [
